777e6bfcd162749fb3fa9e38200358b21938892f
Mario Vallejo <mario.vallejo@milwaukeetool.com>
2025-05-06 14:38:02 -0500

    clean up

--- src/components/Board/Status.jsx
+++ src/components/Board/Status.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { calculateWinner } from '../../helpers';
 import { useSelector } from 'react-redux';
-import { selectCurrentSquares, selectXIsNext } from '../../store/gameSlice';
+import { selectXIsNext } from '../../store/gameSlice';
 
 function Status() {
   const xIsNext = useSelector(selectXIsNext);
-  const squares = useSelector(selectCurrentSquares);
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+  const winner = useSelector(s=>s.game.winner);
 
   return (
     <>
         <h4 className='text-2xl font-bold text-center'>
-          {status}
+          {winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`}
         </h4>
     </>
   );
